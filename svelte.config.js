229ce0fc80b31a2b89e3ce28bdd25b9546a804f0
Salikhova Nazgul <109477146+Nazgulitos@users.noreply.github.com>
2023-07-18 23:04:10 +0300

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,20 @@
-import node from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 
+const dev = process.argv.includes('dev');
+
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: node(),
-	  }
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		},
+
+		adapter: adapter()
+	}
 };
+
 export default config;
