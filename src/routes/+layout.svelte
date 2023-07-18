9a3c2823f67c7d84a2d06ac885b10c69615752fb
natagapova <131772742+natagapova@users.noreply.github.com>
2023-07-18 22:03:48 +0300

    Nat

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,23 @@
+<script>
+    import "../global.css";
+</script>
+
+<header class="header">
+    <img src="$lib/images/logo.png" class="logo" alt="Our logo">
+
+    <nav class="nav-links">
+        <ul>
+            <li>
+                <div class="current"><a href="/">Main page</a></div>
+            </li>
+            <li><a href="/BookCrossingPage">Book crossing</a></li>
+            <li><a href="/BlogPagePublic">Blog</a></li>
+            <li><a href="/EventsPage">Events</a></li>
+            <li><a href="/LoginPage">Log in</a></li>
+            <li><a href="https://t.me/innobooklovers"><img class="telegram" src="images/telegram.png" alt="Our telegram channel link"></a></li>
+        </ul>
+    </nav>
+    
+</header>
+
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import "../main.css";
+</script>
 
+<head><title>InnoBookLovers: Main page</title></head>
 
-Natasha
+<body>
+    <div class="main">
+
+        <div class="imageContainer">
+            <img src="../mainphoto.jpg" class="mainphoto" alt="That's us">
+        </div>
+
+        <div class="textContainer">
+
+            <h1 class="pageName">INNOBOOKLOVERS</h1>
+
+            <div class="whoAreWe">
+                <div class="hd2"><h2>Who are we?</h2></div>
+                <p>We are Innopolis reading club. We share our passion for books and everyone is welcome to join us! Our club is a place for students with passion for reading who are looking for like-minded people.</p>
+            </div>
+
+            <div class="whatIsThis">
+                <div class="hd2"><h2>What is this?</h2></div>
+                <p>This is our website! Here you can find all the information about us, our events, read and post in our blog, and cross your books with other club members!</p>
+            </div>
+
+            <div class="container">
+                <button><a href="#">Sign up</a></button>
+            </div>
+
+        </div>
+
+    </div>
+</body>
