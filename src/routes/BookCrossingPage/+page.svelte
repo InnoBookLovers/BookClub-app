c6dfecc9876f38bf654133ecd704865313965a6c
nikolaevaElizaveta <118183018+nikolaevaElizaveta@users.noreply.github.com>
2023-07-19 23:06:56 +0300

    Update +page.svelte

--- src/routes/BookCrossingPage/+page.svelte
+++ src/routes/BookCrossingPage/+page.svelte
@@ -2,9 +2,9 @@
     import './LizaStyle.css';
     import book_sister_carrie_pic from '$lib/images/book_carry_sister_pic.jpg';
     import book_dorian_gray_pic from '$lib/images/book_dorian_gray_pic.jpg';
-    import book_great_gatsby_pic from '$lib/images/book_great_gatsby_pic.jpg';
-    import book_shining_pic from '$lib/images/book_shining_pic.JPG';
-    import book_flowers_alg_pic from '$lib/images/book_flowers_alg_pic.JPG';
+    //import book_great_gatsby_pic from '$lib/images/book_great_gatsby_pic.jpg';
+    //import book_shining_pic from '$lib/images/book_shining_pic.JPG';
+    //import book_flowers_alg_pic from '$lib/images/book_flowers_alg_pic.JPG';
 
     import Card from './Card.svelte';
 
@@ -27,21 +27,21 @@
             book_name: "The Great Gatsby",
             author: "Francis Scott Fitzgerald",
             language: "Russian",
-            image_url: book_great_gatsby_pic,
+            //image_url: book_great_gatsby_pic,
             social_media_url: "https://web.telegram.org/k/#@nhefy"
         },
 	{
             book_name: "The Shining",
             author: "Stephen King",
             language: "Russian",
-            image_url: book_shining_pic,
+            //image_url: book_shining_pic,
             social_media_url: "https://web.telegram.org/k/#@nhefy"
         },
 	{
             book_name: "Flowers for Algernon",
             author: "Daniel Keyes",
             language: "Russian",
-            image_url: book_flowers_alg_pic,
+            //image_url: book_flowers_alg_pic,
             social_media_url: "https://web.telegram.org/k/#@nhefy"
         },
     ];
